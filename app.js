b2881040931e1e819d1f49c28eb91476a5a167b5
2147483647 <jbhyun2005@naver.com>
2021-12-13 23:11:53 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const file = document.querySelector("#file");
 const fileText = document.querySelector("#fileText");
 const reader = new FileReader();
 let fileName;
-let objtemp;
 //LoadFile
 file.addEventListener("change", (event)=>{
     reader.readAsText(event.target.files[0]);
@@ -19,7 +18,6 @@ function convertFile(){
     let result = "#EXTM3U\n";
     const songs = JSON.parse(fileText.value)["members"];
     console.dir(songs);
-    objtemp=songs;
     for(let i=0;i<songs.length;i++){
         result += objtemp[i]["info"]+"\n";
     }
