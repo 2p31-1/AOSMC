4119ecbc3b643dca6ae1a00aed5973682442632f
2147483647 <jbhyun2005@naver.com>
2021-12-21 13:17:35 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -4,28 +4,28 @@ const fileText = document.querySelector("#fileText");
 const reader = new FileReader();
 let fileName;
 //LoadFile
-file.addEventListener("change", (event)=>{
+file.addEventListener("change", (event) => {
     reader.readAsText(event.target.files[0]);
-    reader.onload = function(){
-        fileText.value=reader.result;
+    reader.onload = function () {
+        fileText.value = reader.result;
     }
     const filePath = event.target.value;
     let result = filePath.split("/");
-    result = result[result.length-1].split("\\");
-    fileName = result[result.length-1]+".m3u8";
+    result = result[result.length - 1].split("\\");
+    fileName = result[result.length - 1] + ".m3u8";
 })
-function convertFile(){
+function convertFile() {
     let result = "#EXTM3U\n";
     const songs = JSON.parse(fileText.value)["members"];
     console.dir(songs);
-    for(let i=0;i<songs.length;i++){
-        result += objtemp[i]["info"]+"\n";
+    for (let i = 0; i < songs.length; i++) {
+        result += songs[i]["info"] + "\n";
     }
     fileText.value = result;
 }
-function downloadFile(){
+function downloadFile() {
     let element = document.createElement('a');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,'+encodeURIComponent(fileText.value));
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(fileText.value));
     element.setAttribute('download', fileName);
     document.body.appendChild(element);
     element.click();
